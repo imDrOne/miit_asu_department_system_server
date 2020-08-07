a34029df8b9609b8173841bacfa5f7a34861bbcf
atiholoz <andre.tikholoz@s-telematics.ru>
2020-08-07 10:52:36 +0300

    add morgan js;

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ require('dotenv').config()
 const exress = require('express')
 const app = exress()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const PORT = process.env.PORT || 4000
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.get('/', (req, res) => res.send('Hello!'))
 app.listen(PORT, () => console.log(PORT))
